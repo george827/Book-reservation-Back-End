a2068c4527a55a07bd98f7350719e8ca8bf075d1
george827 <kinyanjuigeorge827@gmail.com>
2023-05-08 17:14:46 +0300

    add user  controller

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,17 @@
+class APi::V1::UsersController < 
+  def register
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: :created
+    else
+      render json: {errors: user.errors}, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
+ 
+end
