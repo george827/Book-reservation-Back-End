6a494696110f6cf9a71cbaf2d52afce87c0f9f66
george827 <kinyanjuigeorge827@gmail.com>
2023-05-08 17:14:05 +0300

    add sessions controller

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::SessionsController < ApplicationController
+  def login
+    user = User.find_by(name: params[:name])
+    if user
+      session[:user_id] = user.id
+      render json: {success: true, user_id: user.id, user_name: user.name }, status: :ok
+    else
+      render json: {success: false. error: "Invalid username" }, status: :unprocessable_entity
+    end
+  end
+
+  def logout
+    render json: {message: "Logged out"}, status: :ok
+  end
+end
