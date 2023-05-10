3524fa61ab43a051ec07efe4347f3c031245e963
willy836 <wilsonkimiri836@gmail.com>
2023-05-10 13:20:13 +0300

    Update reservations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_08_101028) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_10_101554) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_08_101028) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.bigint "restaurant_table_id", null: false
+    t.string "table_name"
     t.index ["restaurant_table_id"], name: "index_reservations_on_restaurant_table_id"
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
