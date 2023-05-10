32f8a52b6720768721ad8b3375c483973934154c
Harshita Jain <harshitaforever06@gmail.com>
2023-05-10 14:55:08 +0530

    linters corrected

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::SessionsController < ApplicationController
-    def login
-      @user = User.find_by(name: params[:name])
-  
-      if @user
-        session[:user_id] = @user.id
-        render json: { success: true, user_id: @user.id, user_name: @user.name }, status: :ok
-      else
-        render json: { success: false, error: "Invalid username" }, status: :unprocessable_entity
-      end
+  def login
+    @user = User.find_by(name: params[:name])
+
+    if @user
+      session[:user_id] = @user.id
+      render json: { success: true, user_id: @user.id, user_name: @user.name }, status: :ok
+    else
+      render json: { success: false, error: 'Invalid username' }, status: :unprocessable_entity
     end
+  end
 end
