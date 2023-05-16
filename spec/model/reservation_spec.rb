81488f4bacb79630104d1d04a761926a25c4199e
Harshita Jain <harshitaforever06@gmail.com>
2023-05-16 09:56:40 +0530

    Correct linters

--- spec/model/reservation_spec.rb
+++ spec/model/reservation_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe Reservation, type: :model do
   before(:example) do
     @user = User.create(name: 'Dev')
-    @reservation = Reservation.create(user: @user, city: 'Agra', start_date: '15/05/2023', end_date: '16/05/2023', table_name: 'Table6')
+    @reservation = Reservation.create(user: @user, city: 'Agra', start_date: '15/05/2023', end_date: '16/05/2023',
+                                      table_name: 'Table6')
   end
 
   it 'city is invalid' do
@@ -25,5 +26,4 @@ RSpec.describe Reservation, type: :model do
     @reservation.end_date = nil
     expect(@reservation).to_not be_valid
   end
-
 end
--- spec/model/restaurant_table_spec.rb
+++ spec/model/restaurant_table_spec.rb
@@ -3,10 +3,12 @@ require 'rails_helper'
 RSpec.describe RestaurantTable, type: :model do
   before(:example) do
     @user = User.create(name: 'Dev')
-    @restaurant_table = RestaurantTable.create(image: 'photo', name: 'Table6', desc: '15/05/2023', price: '$16', table_size: 6)
-    @reservation = Reservation.create(user: @user, city: 'Agra', start_date: '15/05/2023', end_date: '16/05/2023', table_name: 'Table6')
+    @restaurant_table = RestaurantTable.create(image: 'photo', name: 'Table6', desc: '15/05/2023', price: '$16',
+                                               table_size: 6)
+    @reservation = Reservation.create(user: @user, city: 'Agra', start_date: '15/05/2023', end_date: '16/05/2023',
+                                      table_name: 'Table6')
   end
-  
+
   it 'image is invalid' do
     @restaurant_table.image = nil
     expect(@restaurant_table).to_not be_valid
@@ -31,5 +33,4 @@ RSpec.describe RestaurantTable, type: :model do
     @restaurant_table.table_size = nil
     expect(@restaurant_table).to_not be_valid
   end
-
 end
