ac575f91b70198266b634a3e0a8d0beaed6d76cc
Harshita Jain <harshitaforever06@gmail.com>
2023-05-18 16:08:33 +0530

    Correct linters

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,6 +4,8 @@ class Reservation < ApplicationRecord
 
   validates :city, presence: true, length: { minimum: 3, maximum: 50 }
   validates :table_name, presence: true, length: { minimum: 3, maximum: 50 }
-  validates :start_date, presence: true, format: { with: /\A\d{4}-\d{2}-\d{2}\z/, message: "must be in the format YYYY-MM-DD and have exactly 10 characters" }, length: { is: 10 }
-  validates :end_date, presence: true, format: { with: /\A\d{4}-\d{2}-\d{2}\z/, message: "must be in the format YYYY-MM-DD and have exactly 10 characters" }, length: { is: 10 }
+  validates :start_date, presence: true,
+                         format: { with: /\A\d{4}-\d{2}-\d{2}\z/ }, length: { is: 10 }
+  validates :end_date, presence: true,
+                       format: { with: /\A\d{4}-\d{2}-\d{2}\z/ }, length: { is: 10 }
 end
